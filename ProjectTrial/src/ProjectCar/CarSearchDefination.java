4a52b80fb9909d25d648049d515192b580a42c3d
victoriahao <42991208+victoriahao@users.noreply.github.com>
2018-12-04 11:25:50 -0800

    Update CarSearchDefination.java

--- ProjectTrial/src/ProjectCar/CarSearchDefination.java
+++ ProjectTrial/src/ProjectCar/CarSearchDefination.java
@@ -5,16 +5,12 @@ public class CarSearchDefination extends JFrame{
 	//protected JFrame mainFrame;
 	private static final String JLable = null;
 	protected JPanel leftPanel, vehicleDetailsPanel, topPanel, vehicleDetailsPane;
-	protected JButton homeButton, viewDetails, searchButton, clearFiltersButton, moreButton1, moreButton2, moreButton3, moreButton4, 
-	moreButton5, moreButton6,moreDetails, clearButton, clearButton1, clearButton2, clearButton3, clearButton4, clearButton5, clearButton6;
+	protected JButton homeButton, viewDetails, searchButton, clearFiltersButton, moreDetails, clearButton;
 	protected JLabel topPicture, modelFilterLabel, yearFilterLabel, brandFilterLabel,
 	priceFilterLabel, bodyTypeFilterLabel,categoryFilterLabel, sortLabel, vehicleImageLabel, vehicleModel, vehicleMake,
 	vehicleBodyType, vehiclePrice, vehicleCategory, vehicleYear,searchVehicleLabel;
-	JCheckBox modelFilterA, modelFilterB, modelFilterC, bodyTypeFilter1, bodyTypeFilter2, bodyTypeFilter3, bodyTypeFilter4,
-	priceFiltera, priceFilterb, priceFilterc, priceFilterd, priceFiltere, priceFilterf, priceFilterg, 
-	brandFilter1, brandFilter2, brandFilter3, brandFilter4, yearFilter1, yearFilter2, yearFilter3,categoryFilter1,
-	categoryFilter2, categoryFilter3, categoryFilter4;
-	protected JComboBox yearFilter, brandFilter, priceFilter, bodyTypeFilter, categoryFilter, sort;
+	protected JCheckBox categoryFilter1, categoryFilter2, categoryFilter3, categoryFilter4;
+	protected JComboBox yearFilter, brandFilter, modelFilter, priceFilter, bodyTypeFilter, categoryFilter, sort;
 	protected JTextField searchBar;
 	protected JScrollPane scrollPane,leftscrollPane;
 	protected Container container;
